1b40358254c2ad80bdf68758aff2a2a8f54f5664
zeroflag <m.magyar3@gmail.com>
2025-12-12 00:31:25 +0100

    tiles wip

--- io/display.c
+++ io/display.c
@@ -4,12 +4,25 @@ SDL_Renderer *renderer;
 SDL_Window   *window;
 
 const char TITLE[] = "WarpCore";
-const int WIDTH  = 512;
-const int HEIGHT = 512;
 const int FPS = 60;
+
+const int TILE_WIDTH  = 8;
+const int TILE_HEIGHT = 8;
+const int TILE_SIZE   = TILE_WIDTH * TILE_HEIGHT;
+
+const int N_TILES_X = 40;
+const int N_TILES_Y = 30;
+
+const int WIDTH  = N_TILES_X * TILE_WIDTH;
+const int HEIGHT = N_TILES_Y * TILE_HEIGHT;
+
+const int VRAM = 0x3000;
+const int TRAM = 0x4000;
 
 Uint32 last_rendered;
 Uint32 threshold = 1000 / FPS;
+
+SDL_Texture* framebuffer;
 
 void sdl_init() {
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -17,7 +30,7 @@ void sdl_init() {
       return;
   }
 
-  SDL_Window *window = SDL_CreateWindow(
+  window = SDL_CreateWindow(
     TITLE,
     SDL_WINDOWPOS_CENTERED,
     SDL_WINDOWPOS_CENTERED,
@@ -40,27 +53,40 @@ void sdl_init() {
     SDL_Quit();
     return;
   }
+
+  framebuffer = SDL_CreateTexture(
+      renderer,
+      SDL_PIXELFORMAT_RGBA8888,
+      SDL_TEXTUREACCESS_STREAMING,
+      WIDTH, HEIGHT
+  );
+}
+
+void draw_tile(uint8_t* mem, int tile_base, int x, int y) {
+  uint8_t* pixels;
+  int pitch;
+
+  SDL_LockTexture(framebuffer, NULL, (void**)&pixels, &pitch);
+
+  for (int ty = 0; ty < TILE_HEIGHT; ty++) {
+    for (int tx = 0; tx < TILE_WIDTH; tx++) {
+      uint8_t color = mem[tile_base + ty * TILE_WIDTH + tx];
+      uint32_t* p = (uint32_t*)(pixels + (y + ty) * pitch)
+                    + (x + tx);
+      *p = color;
+    }
+  }
+
+  SDL_UnlockTexture(framebuffer);
 }
 
 void sdl_render(uint8_t* mem) {
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
-
-  for (int x = 0; x < WIDTH / 8; x++) {
-    for (int y = 0; y < HEIGHT / 8; y++) {
-
-      uint8_t color = mem[0x3000 + y * (WIDTH / 8) + x];
-      uint8_t r = (color >> 5) & 0x07; // 3 bits
-      uint8_t g = (color >> 2) & 0x07; // 3 bits
-      uint8_t b = color & 0x03;        // 2 bits
-
-      r = r * 255 / 7;
-      g = g * 255 / 7;
-      b = b * 255 / 3;
-
-      SDL_Rect outline = {x * 8, y * 8, 8, 8};
-      SDL_SetRenderDrawColor(renderer, 0, r, g, b);
-      SDL_RenderFillRect(renderer, &outline);
+  for (int tx = 0; tx < N_TILES_X; tx++) {
+    for (int ty = 0; ty < N_TILES_Y; ty++) {
+      uint8_t tile_index = mem[VRAM + (ty * N_TILES_X) + tx];
+      draw_tile(mem, TRAM + (tile_index * TILE_SIZE), tx, ty);
     }
   }
   SDL_RenderPresent(renderer);
